477123abd641d1ca5f3fc8ea4d109e30cd877011
Unknown <aronmar@live.no>
2019-08-22 17:24:32 +0200

    router init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-let test = "<h1>Hello World!</h1>";
+// Set up router
+const router = new Router();
+// The different routes
+router.get('/test', () => {
+    console.log("You are on route /test!!");
+});
 
-window.onload = renderContentWithNav(newProduct());
+router.get('/', function(req) {
+    console.log("Your path:", req.path);
+});
+// Init
+router.init();
+
+const routes = {
+    '/home/aronmn/Development/NTNU_MobDist/Oppgave1/public/index.html': homepage(),
+    '/home/aronmn/Development/NTNU_MobDist/Oppgave1/public/index.html/test': '<h1>Hello World</h1>'
+}
+
+console.log(window.location.pathname);
+
+document.getElementById('root').innerHTML = routes[window.location.pathname];
+
+//window.onload = renderContentWithNav(newProduct());
 
 // Materialize initialize stuff (TODO put into own init.js file)
 M.Sidenav.init(document.querySelector('.sidenav'), {});
